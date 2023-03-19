d5488aa14e097e3cb81ec107e153f0c028f17591
Henri Podolski <henrip@gmail.com>
2023-03-19 19:53:47 +0100

    Remove previous implementation of state management

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -1,20 +1,19 @@
-import React, { useContext } from 'react';
-import { GlobalContext } from '../../context/global.context';
+import React from 'react';
 import styles from './Preview.module.scss';
 import cx from 'classnames';
+import { useRecoilValue } from 'recoil';
+import { openFileState } from '../../store/openFile/openFile.atoms';
 
 export type Props = {
   className: string;
 };
 
 const Preview: React.FC<Props> = ({ className }: Props) => {
-  // Todo use selector of open file content that converts content
-  // to HTML
-  const [globalContext] = useContext(GlobalContext);
+  const openFile = useRecoilValue(openFileState);
   return (
     <div
       className={cx(styles.Preview, className)}
-      dangerouslySetInnerHTML={{ __html: globalContext.html }}
+      dangerouslySetInnerHTML={{ __html: openFile?.content ?? '' }}
     ></div>
   );
 };
