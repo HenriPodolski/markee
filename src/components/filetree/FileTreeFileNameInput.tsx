01c69896fb9366b220eb7011328b0f678b44c261
Henri Podolski <henrip@gmail.com>
2023-04-08 08:19:53 +0200

    WIP Implement file/folder name form

--- src/components/filetree/FileTreeFileNameInput.tsx
+++ src/components/filetree/FileTreeFileNameInput.tsx
@@ -1,19 +1,52 @@
-import React, { FocusEvent, FunctionComponent } from 'react';
+import React, { FormEvent, FunctionComponent } from 'react';
 import styles from './FileTreeFileNameInput.module.scss';
 import { ReactComponent as FileIcon } from '../../icons/file-document-outline.svg';
 import { useScrollIntoViewOnMount } from '../../lib/hooks/useScrollIntoViewOnMount';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import { appState } from '../../store/app/app.atoms';
+import { uuid } from '../../lib/uuid';
+import { FileSystemTypeEnum } from '../../store/fileSystem/fileSystem.enums';
+import { fileSystemState } from '../../store/fileSystem/fileSystem.atoms';
 
 interface Props {}
 
 const FileTreeFileNameInput: FunctionComponent<Props> = () => {
   const [app, setApp] = useRecoilState(appState);
+  const setFileSystem = useSetRecoilState(fileSystemState);
   const elementRef = useScrollIntoViewOnMount<HTMLFormElement>();
 
-  const handleBlur = (evt: FocusEvent<HTMLInputElement>) => {
-    if (evt.target.value) {
-      console.log('save', evt.target.value);
+  const handleBlur = () => {
+    if (elementRef.current) {
+      elementRef.current.submit();
+    }
+  };
+
+  const handleSubmit = (evt: FormEvent) => {
+    evt.preventDefault();
+
+    const formData = new FormData(evt.target as HTMLFormElement);
+    const fileName = formData.get('file') as string;
+
+    // Todo: check if file name already exists in file system on that level
+    if (fileName) {
+      const extension = fileName.endsWith('.md') ? '' : '.md';
+      const newFile = {
+        name: `${fileName}${extension}`,
+        id: uuid(),
+        fullPath: `${app?.createFile}/${fileName}${extension}`,
+        basePath: app?.createFile as string,
+        type: FileSystemTypeEnum.file,
+        visible: true,
+        open: true,
+        active: true,
+        level: `${app?.createFile}/${fileName}${extension}`.split('/')
+          .length as number,
+        modified: new Date(),
+      };
+
+      // Todo: implement an effect that syncs the folder from the
+      //  virtual filesystem to the persistent filesystem
+      setFileSystem((prevState) => [...prevState, newFile]);
     }
 
     setApp({
@@ -23,7 +56,11 @@ const FileTreeFileNameInput: FunctionComponent<Props> = () => {
   };
 
   return (
-    <form ref={elementRef} className={styles.FileTreeFileNameInput}>
+    <form
+      onSubmit={handleSubmit}
+      ref={elementRef}
+      className={styles.FileTreeFileNameInput}
+    >
       <FileIcon />
       <input
         type="text"
--- src/components/filetree/FileTreeFolderNameInput.tsx
+++ src/components/filetree/FileTreeFolderNameInput.tsx
@@ -1,19 +1,50 @@
-import React, { FocusEvent, FunctionComponent } from 'react';
+import React, { FormEvent, FunctionComponent } from 'react';
 import styles from './FileTreeFolderNameInput.module.scss';
 import { ReactComponent as FolderIcon } from '../../icons/folder.svg';
 import { useScrollIntoViewOnMount } from '../../lib/hooks/useScrollIntoViewOnMount';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import { appState } from '../../store/app/app.atoms';
+import { fileSystemState } from '../../store/fileSystem/fileSystem.atoms';
+import { FileSystemTypeEnum } from '../../store/fileSystem/fileSystem.enums';
+import { uuid } from '../../lib/uuid';
 
 interface Props {}
 
 const FileTreeFolderNameInput: FunctionComponent<Props> = () => {
   const [app, setApp] = useRecoilState(appState);
+  const setFileSystem = useSetRecoilState(fileSystemState);
   const elementRef = useScrollIntoViewOnMount<HTMLFormElement>();
 
-  const handleBlur = (evt: FocusEvent<HTMLInputElement>) => {
-    if (evt.target.value) {
-      console.log('save', evt.target.value);
+  const handleBlur = () => {
+    if (elementRef.current) {
+      elementRef.current.submit();
+    }
+  };
+
+  const handleSubmit = (evt: FormEvent) => {
+    evt.preventDefault();
+
+    const formData = new FormData(evt.target as HTMLFormElement);
+    const folderName = formData.get('folder');
+
+    // Todo: check if folder name already exists in file system on that level
+    if (folderName) {
+      const newFolder = {
+        name: folderName as string,
+        id: uuid(),
+        fullPath: `${app?.createFolder}/${folderName}`,
+        basePath: app?.createFolder as string,
+        type: FileSystemTypeEnum.directory,
+        visible: true,
+        open: true,
+        active: true,
+        level: `${app?.createFolder}/${folderName}`.split('/').length as number,
+        modified: new Date(),
+      };
+
+      // Todo: implement an effect that syncs the folder from the
+      //  virtual filesystem to the persistent filesystem
+      setFileSystem((prevState) => [...prevState, newFolder]);
     }
 
     setApp({
@@ -23,16 +54,21 @@ const FileTreeFolderNameInput: FunctionComponent<Props> = () => {
   };
 
   return (
-    <form ref={elementRef} className={styles.FileTreeFolderNameInput}>
+    <form
+      onSubmit={handleSubmit}
+      ref={elementRef}
+      className={styles.FileTreeFolderNameInput}
+    >
       <FolderIcon />
       <input
         type="text"
         name="folder"
         onBlur={handleBlur}
         autoFocus={true}
+        autoComplete="off"
         placeholder="Enter folder name..."
         required
-        pattern="[a-zA-Z0-9][a-zA-Z0-9_-]*"
+        pattern="[a-zA-Z0-9_-]*"
       />
     </form>
   );
