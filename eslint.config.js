8ac72bbb1c937c16b783204a21b80285b2f54c12
Henri Podolski <henrip@gmail.com>
2025-01-31 21:20:23 +0100

    Linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -9,10 +8,10 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
     },
+    ignores: ['backup', 'src/lib/preact-custom-element.ts'],
   },
 ];
