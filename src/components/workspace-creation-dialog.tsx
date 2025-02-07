bb29f74cf05864bf95b59d31e31c0bd3e66931bb
Henri Podolski <henrip@gmail.com>
2025-02-07 20:40:12 +0100

    Add create workspace function

---
+++ src/components/workspace-creation-dialog.tsx
@@ -0,0 +1,75 @@
+import { Button } from "./ui/button"
+import {
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "./ui/dialog"
+import { Input } from "@/editor/registry/new-york/ui//input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel, FormMessage
+} from "./ui/form.tsx";
+import {useWorkspaces} from "../store/store.ts";
+import {SetStateAction} from "react";
+
+const formSchema = z.object({
+    title: z.string().min(3, {
+        message: "Workspace title must be at least 3 characters.",
+    }).max(40, {
+        message: "Workspace title must not be longer then 40 characters.",
+    }),
+})
+
+export function WorkspaceCreationDialog({ setWorkspaceCreationDialogOpen }: { setWorkspaceCreationDialogOpen: SetStateAction<boolean> }) {
+    const { createWorkspace } = useWorkspaces();
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+        },
+    });
+
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        await createWorkspace(`/${values.title}`);
+        setWorkspaceCreationDialogOpen(false);
+    }
+
+    return (
+        <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+                <DialogTitle>Add workspace</DialogTitle>
+                <DialogDescription>
+                    Create a new workspace to organize your notes better
+                </DialogDescription>
+            </DialogHeader>
+            <Form {...form}>
+                <form className="grid gap-4 py-4" onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem className="grid grid-cols-4 items-center gap-4">
+                                <FormLabel>Title</FormLabel>
+                                <FormControl>
+                                    <Input className="col-span-3" placeholder="Type in a title for your workspace" {...field} />
+                                </FormControl>
+                                <FormMessage className="col-span-3" />
+                            </FormItem>
+                        )}
+                    />
+                    <DialogFooter>
+                        <Button type="submit">Save workspace</Button>
+                    </DialogFooter>
+                </form>
+            </Form>
+        </DialogContent>
+    )
+}
--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -15,6 +15,8 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar"
 import {useState} from "react";
+import {WorkspaceCreationDialog} from "./workspace-creation-dialog.tsx";
+import {Dialog} from "./ui/dialog.tsx";
 
 export function WorkspaceSwitcher({
   workspaces,
@@ -25,59 +27,59 @@ export function WorkspaceSwitcher({
 }) {
   const { isMobile } = useSidebar();
   const [activeWorkspace, setActiveWorkspace] = useState(workspaces[0]);
-
-  const handleWorkspaceAdd = (evt: MouseEvent) => {
-    evt.preventDefault();
-    console.log('handleWorkspaceAdd');
-  }
+  const [workspaceCreationDialogOpen, setWorkspaceCreationDialogOpen] = useState(false);
 
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">
-                  {activeWorkspace}
-                </span>
-              </div>
-              <ChevronsUpDown className="ml-auto" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-            align="start"
-            side={isMobile ? "bottom" : "right"}
-            sideOffset={4}
-          >
-            <DropdownMenuLabel className="text-xs text-muted-foreground">
-              Workspaces
-            </DropdownMenuLabel>
-            {workspaces.map((workspace, index) => (
-              <DropdownMenuItem
-                key={workspace}
-                onClick={() => setActiveWorkspace(workspace)}
-                className="gap-2 p-2"
+        <Dialog open={workspaceCreationDialogOpen}
+                onOpenChange={(open: boolean) => setWorkspaceCreationDialogOpen(open) }>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
               >
-                {workspace}
-                <DropdownMenuShortcut>⌘{index + 1}</DropdownMenuShortcut>
-              </DropdownMenuItem>
-            ))}
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="gap-2 p-2">
-              <button type="button" onClick={handleWorkspaceAdd} className="appearance-none contents">
-                <div className="flex size-6 items-center justify-center rounded-md border bg-background">
-                  <Plus className="size-4" />
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">
+                    {activeWorkspace}
+                  </span>
                 </div>
-                <div className="font-medium text-muted-foreground">Add workspace</div>
-              </button>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+                <ChevronsUpDown className="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+              align="start"
+              side={isMobile ? "bottom" : "right"}
+              sideOffset={4}
+            >
+              <DropdownMenuLabel className="text-xs text-muted-foreground">
+                Workspaces
+              </DropdownMenuLabel>
+              {workspaces.map((workspace, index) => (
+                <DropdownMenuItem
+                  key={workspace}
+                  onClick={() => setActiveWorkspace(workspace)}
+                  className="gap-2 p-2"
+                >
+                  {workspace}
+                  <DropdownMenuShortcut>⌘{index + 1}</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              ))}
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="gap-2 p-2">
+                <button onClick={() => setWorkspaceCreationDialogOpen(true)} type="button" className="appearance-none contents">
+                  <div className="flex size-6 items-center justify-center rounded-md border bg-background">
+                    <Plus className="size-4" />
+                  </div>
+                  <div className="font-medium text-muted-foreground">Add workspace</div>
+                </button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <WorkspaceCreationDialog setWorkspaceCreationDialogOpen={setWorkspaceCreationDialogOpen} />
+        </Dialog>
       </SidebarMenuItem>
     </SidebarMenu>
   )
